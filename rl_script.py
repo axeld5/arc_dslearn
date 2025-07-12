f5a2e59485692c7c837eece80273d476257a0f4e
axeld5 <axeldarmouni@gmail.com>
2025-07-13 01:00:44 +0200

    update rl script tokenizer

--- rl_script.py
+++ rl_script.py
@@ -43,6 +43,9 @@ tokenizer = AutoTokenizer.from_pretrained(
     trust_remote_code=True
 )
 
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
 # ---------------------------------------------------------------------
 # 2. Loading model  (LoRA)
 # ---------------------------------------------------------------------
